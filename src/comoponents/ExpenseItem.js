4cbc81ec7666f7ea8d4d55dbe7be56976bb5f119
sachinraj31 <120715367+sachinraj31@users.noreply.github.com>
2023-03-25 11:18:22 +0530

    intial commit

---
+++ src/comoponents/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>Epense Item</h2>
+      <h2>Foods Rs 10</h2>
+      <h2>Petrol Rs 100</h2>
+      <h2>Movies Rs 200</h2>
+    </div>
+  );
+}
+
+export default ExpenseItem;
