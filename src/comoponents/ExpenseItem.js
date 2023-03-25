a591e19e9ff7b946c01fe730a1d3e2a9451abab9
sachinraj31 <120715367+sachinraj31@users.noreply.github.com>
2023-03-25 14:48:55 +0530

    location of expenditure

--- src/comoponents/ExpenseItem.js
+++ src/comoponents/ExpenseItem.js
@@ -1,10 +1,12 @@
 function ExpenseItem() {
+  const LocationOfexpenditure = Hotel;
   return (
     <div>
       <h2>Epense Item</h2>
       <h2>Foods Rs 10</h2>
       <h2>Petrol Rs 100</h2>
       <h2>Movies Rs 200</h2>
+      <div className="locaton">{LocationOfexpenditure}</div>
     </div>
   );
 }
